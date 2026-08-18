a6374cf6011294b0630a38d3745d23673f4b2fc6
Oğulcan Kacar <ogulcankacarr@gmail.com>
2026-08-18 18:27:59 +0300

    Faz 0: EF Core + AppDbContext + Postgres baglantisi

---
+++ src/PaymentGateway.Api/Data/AppDbContext.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using PaymentGateway.Api.Models.Entities;
+
+namespace PaymentGateway.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Payment> Payments => Set<Payment>();
+    public DbSet<Merchant> Merchants => Set<Merchant>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Payment>(payment =>
+        {
+            payment.Property(p => p.Amount).HasPrecision(18, 2); // 18 basamaklı, 2 ondalık
+            payment.Property(p => p.Status).HasConversion<string>();
+            payment.Property(p => p.Currency).HasMaxLength(3);
+            payment.Property(p => p.CardLast4).HasMaxLength(4);
+            
+        });
+        
+        modelBuilder.Entity<Merchant>(merchant =>
+        {
+            merchant.HasIndex(m => m.ApiKey).IsUnique();
+        });
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        ApplyAuditFields();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+    
+    public override int SaveChanges()
+    {
+        ApplyAuditFields();
+        return base.SaveChanges();
+    }
+
+
+
+
+    private void ApplyAuditFields()
+    {
+        var entries = ChangeTracker.Entries<BaseEntity>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+            }
+            
+            
+        }
+    }
+}
