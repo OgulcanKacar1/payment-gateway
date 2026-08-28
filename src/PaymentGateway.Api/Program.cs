f158c6a13a5d7b77a61ec972bc186799e766b14c
Oğulcan Kacar <ogulcankacarr@gmail.com>
2026-08-28 17:08:26 +0300

    Deploy: PORT env degiskenini dinle (cloud icin)

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -6,6 +6,9 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
